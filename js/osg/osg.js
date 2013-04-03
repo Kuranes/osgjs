c517ae10e4c64e5842991a5f56581a17ad112aef
Kuranes <tuan.kuranes@gmail.com>
2013-04-03 19:06:20 +0200

    Correctly Merge branch 'develop' of https://github.com/cedricpinson/osgjs into feature/shadows
    Conflicts:
    	js/osg/osg.js

--- js/osg/osg.js
+++ js/osg/osg.js
@@ -217,8 +217,9 @@ osg.objectInehrit = osg.objectInherit = function(base, extras) {
     function F() {}
     F.prototype = base;
     var obj = new F();
-    if(extras) {
-        osg.objectMix(obj, extras, false);
+    // let augment object with multiple arguement
+    for (var i = 1; i < arguments.length; i++) {
+        osg.objectMix(obj, arguments[i], false); 
     }
     return obj;
 };
